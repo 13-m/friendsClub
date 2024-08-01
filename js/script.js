26610357bc821fa8fd203ea60acbb27a2fdb7948
Dan <52635228+DANPaRUS@users.noreply.github.com>
2024-08-01 12:47:06 +0300

    feat: add burger menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector(".burger");
+const burgerMenu = document.getElementById("burgerMenu");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
+  burgerMenu.classList.toggle("visible");
 });
